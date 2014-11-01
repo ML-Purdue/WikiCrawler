ec66147919524f2e00e9c45e6402143319f8e32e
unknown <arya0@k9-04.ad.cs.purdue.edu>
2014-11-01 16:12:08 -0400

    BFS

--- src/runner/Main.java
+++ src/runner/Main.java
@@ -2,6 +2,8 @@ package runner;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 import org.jsoup.Jsoup;
@@ -14,45 +16,77 @@ import config.Config;
 public class Main {
 
 	public static void main(String[] args) {
+		Tile goal;
 		if(Config.DEBUG){
-			startSearch(Config.DEBUG_START_URL, Config.DEBUG_END_URL);
+			goal=startSearch(Config.DEBUG_START_URL, Config.DEBUG_END_URL);
 		}else{
 			Scanner scan = new Scanner(System.in);		
 			System.out.println("Start Link: ");
 			String start = scan.nextLine();
 			System.out.println("End Link: ");
 			String end = scan.nextLine();
-			startSearch(start, end);
+			goal=startSearch(start, end);
+		}
+		Tile curTile=goal;
+		System.out.println("FOUND!!!");
+		while(curTile!=null)
+		{
+			System.out.println(curTile.getUrl());
+			curTile=curTile.getParent();
 		}
 	}
 
-	public static void startSearch(String start, String end)
-	{
-		Document doc = null;
-		try {
-			doc = Jsoup.connect(start).get();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
+	public static Tile startSearch(String start, String end)
+	{	
+		TileList tiles=new TileList();
+		tiles.add(new Tile(start,null,0));
+		for(int i=0;i<tiles.size();i++){
+			Tile tile=tiles.get(i);
+			if(tile.getUrl().equals(end)){
+				return tile;
+			}else{
+				Document doc = getDocument(tile.getUrl());
+				if(doc==null)
+					continue;
+				ArrayList<String> tempURLs=getURLsFromDocument(doc);
+				sanitizeLinks(tempURLs);
+				tiles.addAll(generateTiles(tile,tempURLs));
+			}
 		}
-		Elements links = doc.select("a");
-		ArrayList<String> URL = new ArrayList<String>();
+		return null;
+	}
+	private static ArrayList<Tile> generateTiles(Tile parent,ArrayList<String> tempURLs) {
+		ArrayList<Tile> tiles=new ArrayList<Tile>();
+		for(String url:tempURLs){
+			tiles.add(new Tile(url,parent,parent.getCost()+1));
+		}
+		return tiles;
+	}
 
+	public static Document getDocument(String url){
+		try {
+			return  Jsoup.connect(url).get();
+		} catch (IOException e) {
+			System.err.println("Error on "+url);
+		}
+		return null;
+	}
+
+	public static ArrayList<String> getURLsFromDocument(Document doc){
+		ArrayList<String> URLs = new ArrayList<String>();
+		Elements links = doc.select("a");
 		for (Element link : links)
 		{
-			URL.add(link.absUrl("href"));
+			URLs.add(link.absUrl("href"));
 		}
-		sanitizeLinks(URL);
+		return URLs;
 	}
-
 	public static void sanitizeLinks(ArrayList<String> links){
 		for (int i = 0; i < links.size(); i++){
 			if (!links.get(i).startsWith(Config.BASE_URL)){
 				links.remove(i);
 				i--;
 			}
-			if(i>=0)
-				System.out.println(links.get(i));
 		}		
 	}
 }
